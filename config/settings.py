b57f57520e193a43cc9836c2ab6fead5edff21e4
briggirl2 <briggirl2@gmail.com>
2020-04-26 03:51:44 +0900

    #16.3 verify email 2

--- config/settings.py
+++ config/settings.py
@@ -141,7 +141,7 @@ MEDIA_URL = "/media/"
 EMAIL_HOST = "smtp.mailgun.org"
 EMAIL_PORT = "587"
 EMAIL_HOST_USER = os.environ.get("MAILGUN_USERNAME")
-EMAIL_HOST_PASSOWRD = os.environ.get("MAILGUN_PASSWORD")
+EMAIL_HOST_PASSWORD = os.environ.get("MAILGUN_PASSWORD")
 EMAIL_FROM = "sexy-guy@sandbox657d0855f0064ddeb7f8d3ab82fe8033.mailgun.org"
 
 
