072c4d6664d0df061e76e2bdd0da02724c8ec4dd
briggirl2 <briggirl2@gmail.com>
2020-04-25 20:33:41 +0900

    #15.1 Signup Form 2

--- users/views.py
+++ users/views.py
@@ -35,3 +35,12 @@ class SignupView(FormView):
         "last_name": "Serr",
         "email": "itn@las.com",
     }
+
+    def form_valid(self, form):
+        form.save()
+        email = form.cleaned_data.get("email")
+        password = form.cleaned_data.get("password")
+        user = authenticate(self.request, username=email, password=password)
+        if user is not None:
+            login(self.request, user)
+            return super().form_valid(form)
