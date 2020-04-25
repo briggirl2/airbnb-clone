00afc9241a3af2409b4fb17d33f04f2ecc0fc212
briggirl2 <briggirl2@gmail.com>
2020-04-25 20:17:04 +0900

    #15.0 Signup Form

--- users/views.py
+++ users/views.py
@@ -25,3 +25,13 @@ def log_out(request):
     logout(request)
     return redirect(reverse("core:home"))
 
+
+class SignupView(FormView):
+    template_name = "users/signup.html"
+    form_class = forms.SignUpForm
+    success_url = reverse_lazy("core:home")
+    initial = {
+        "first_name": "Nicolas",
+        "last_name": "Serr",
+        "email": "itn@las.com",
+    }
